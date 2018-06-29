33139909c4125d95d1f5e13a8f89705e7515e67e
William De la Cruz <wdelacruz@rappi.com>
2018-06-29 00:40:16 -0500

    added stats apartment detail service

--- api/routes/apartment.routes.js
+++ api/routes/apartment.routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import { getVulnerabilityGraphData, getSpectrumGraphData} from '../controllers/apartment.controller'
+import { getVulnerabilityGraphData, getSpectrumGraphData, getVulnerabilityStats } from '../controllers/apartment.controller'
 
 let router = Router()
 
 router.get('/apartment/vulnerability-graph', ({ query }, res) => {
     res.json(getVulnerabilityGraphData(query))
+})
+
+router.get('/apartment/vulnerability-stats', ({ query }, res) => {
+    res.json(getVulnerabilityStats(query))
 })
 
 router.get('/apartment/spectrum-graph', ({ query }, res) => {
--- api/services/apartment.js
+++ api/services/apartment.js
@@ -57,6 +57,59 @@ var getRandom = (index, masonry, floors, zone) => {
 	return Math.round(valor * 10000) / 10000;
 }
 
+var getVulnerabilityInd = (masonry, floors, zone) => {
+    var piso = floors
+    var material = masonry
+    var zona = zone
+
+	var M = new Float32Array([0, 4.01, 4.18, 4.64, 5.19, 5.38]);
+	var A = new Float32Array([0, 3.68, 3.71, 3.80, 3.91, 3.95]);
+	var samax = new Float32Array([0, 1.537, 1.003, .745, .572, .440]);
+	var T0 = new Float32Array([0, .08, .12, .17, .22, .27]);
+
+	var M3 = new Float32Array([0, 4.28, 4.39, 4.83, 5.51, 6.37]);
+	var A3 = new Float32Array([0, 3.73, 3.75, 3.84, 3.97, 4.15]);
+	var samax3 = new Float32Array([0, 1.84, 1.15, .83, .64, .52]);
+	var T03 = new Float32Array([0, .08, .13, .18, .23, .28]);
+
+
+	var c = new Float32Array([0, .08, .16, .2]);
+	var a0 = new Float32Array([0, .02, .04, .05]);
+	var ta = new Float32Array([0, .2, .3, .6]);
+	var tb = new Float32Array([0, .6, 1.5, 2.9]);
+	var r = new Float32Array([0, .5, .66, 1]);
+	
+
+	if (material == 3) {
+		if (0 <= T03[piso] && T03[piso] <= ta[zona]) {
+			var valorI = (a0[zona] + (c[zona] - a0[zona])) * (T03[piso] / ta[zona]);
+		}
+		else if (ta[zona] < T03[piso] && T03[piso] <= tb[zona]) {
+			var valorI = c[zona];
+		}
+		else {
+			var valorI = Math.pow((tb[zona] / T03[piso]), r[zona]) * c[zona];
+		}
+
+		var InD = (1 - Math.exp(((-1 * A3[piso]) * Math.pow((valorI / samax3[piso]), M3[piso]))));
+	}
+	else if (material == 2) {
+		if (0 <= T0[piso] && T0[piso] <= ta[zona]) {
+			var valorI = (a0[zona] + (c[zona] - a0[zona])) * (T0[piso] / ta[zona]);
+		}
+		else if (ta[zona] < T0[piso] && T03[piso] <= tb[zona]) {
+			var valorI = c[zona];
+		}
+		else {
+            var valorI = Math.pow((tb[zona] / T0[piso]), r[zona]) * c[zona];
+		}
+        
+        var InD = (1 - Math.exp(((-1 * A[piso]) * Math.pow((valorI / samax[piso]), M[piso]))));
+	}
+
+	return InD
+}
+
 var getZona = (posicion, zone) => {
 
 	var tiempo = new Float32Array([.05, .1, .2, .3, .4, .5, .6, .7, .8, .9, 1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2, 2.2, 2.4, 2.6, 2.8, 3, 3.2, 3.4, 3.6, 3.8, 4, 4.2, 4.4, 4.6, 4.8, 5]);
@@ -134,4 +187,44 @@ var spectrumGraphService = (zone) => {
 	];
 }
 
-export { vulnerabilityGraphService, spectrumGraphService }
+var analysisStatsService = (masonry, floors, zone, number, meters) => {
+    var InD = getVulnerabilityInd(masonry, floors, zone)
+    
+    var result = { ind: InD}
+
+    if (InD == 0) {
+		result.nd = "NULO";
+		result.nv = "NULA";
+		result.nvCm2 = 8244;
+		result.cost = InD * 8244 * number * meters;
+	}
+	else if ((InD > 0) && (InD <= .1)) {
+		result.nd = "LIGERO";
+		result.nv = "BAJA";
+		result.nvCm2 = 8244;
+		result.cost = InD * 8244 * number * meters;
+	}
+	else if ((InD > .1) && (InD <= .15)) {
+		result.nd = "MEDIO";
+		result.nv = "MEDIA";
+		result.nvCm2 = 8244;
+		result.cost = InD * 8244 * number * meters;
+
+	}
+	else if ((InD > .15) && (InD <= .35)) {
+		result.nd = "SEVERO";
+		result.nv = "ALTA";
+		result.nvCm2 = 8244;
+		result.cost = InD * 8244 * number * meters;
+	}
+	else {
+        result.nd = "TOTAL";
+		result.nv = "MUY ALTA";
+		result.nvCm2 = 8244;
+		result.cost = InD * 8244 * number * meters;
+    }
+    
+    return result
+}
+
+export { vulnerabilityGraphService, spectrumGraphService, analysisStatsService }
